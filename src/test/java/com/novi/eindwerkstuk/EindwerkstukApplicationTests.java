5d89a35b3ef1aabc20a6dda94b8aee731165122a
olafv <olafvdberg@hotmail.com>
2021-04-26 12:13:29 +0200

    package's

---
+++ src/test/java/com/novi/eindwerkstuk/EindwerkstukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novi.eindwerkstuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindwerkstukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
