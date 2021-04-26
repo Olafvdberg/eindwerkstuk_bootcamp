d811b16a005367692b46056d0448f25361a37559
olafv <olafvdberg@hotmail.com>
2021-04-26 11:01:55 +0200

    test

---
+++ src/main/java/com/novi/eindwerkstuk/EindwerkstukApplication.java
@@ -0,0 +1,13 @@
+package com.novi.eindwerkstuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindwerkstukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EindwerkstukApplication.class, args);
+	}
+
+}
